280ca4179582169f4514f4a1f42725fdb8d1728d
Muhammad Hassan <75063791+Muhammad-Hassan2@users.noreply.github.com>
2021-06-23 15:13:32 +0500

    here is the files

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!-- <?php
+//variable setting
+// $name = $_REQUEST['name'];
+// $email = $_REQUEST['email'];
+// $message = $_REQUEST['message'];
+
+//check input fields
+// if (empty($name) || empty($email) || empty($message))
+// {
+//     echo "Please fill all the Fields";
+// }
+// else {
+//     mail("mhassan302001@gmail.com", "Email form", $message, "form:$name < $email>");
+//     echo "<script type='text/javascript'>alert('your message sent successfully')
+//     window.history.log (-1);
+//     </script>";
+// }
+?> -->
